20336934bcd049fa4f92ea20c4c737c8b7368f90
JoanWaithira <joanwaithira.jw@gmail.com>
2023-08-24 02:01:57 -0700

    TAsk 1

--- free_stack.c
+++ free_stack.c
@@ -1,18 +1,21 @@
 #include "monty.h"
-
-/* Free the memory of the stack */
+/**
+ * free_stack - A function that frees the stack.
+ * stack - Stack in question
+ * Return: Memory freed
+ */
 void free_stack(stack_t **stack)
 {
-    stack_t *current = *stack;
-    stack_t *next;
-
-    while (current)
-    {
-        next = current->next;
-        free(current);
-        current = next;
-    }
-
-    *stack = NULL;
+	stack_t *current = *stack;
+	stack_t *next;
+	
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	
+	*stack = NULL;
 }
 
